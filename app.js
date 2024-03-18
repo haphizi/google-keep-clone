9b8a0437feac353479b083e66703695e235d853a
haphizi <61571184+haphizi@users.noreply.github.com>
2024-03-18 15:06:13 +0800

    feat: display notes and resets form values after submission
    displayNotes() checks for conditional if note has values entered, create template literals for
    mapping each note el within array, add closeForm() upon submission and resets form values

--- app.js
+++ app.js
@@ -1,14 +1,28 @@
 /* 
  1. Expand and close notes(form)
- 2. Storing notess in the app
- 
+    - toggle styling using openForm()/closeForm() functions
+ 2. Storing notes in the app
+    - capture values during submission
+    - prevent app from reloading
+ 3. Displaying notes in app
+    - clear out notes when submitted, and display it out
+    - call displayNotes() when submitted
+    - map notes[] with styling
+    - reset form title/text and invoke closeForm() after submitting
+ 4. Improving functionality
+    - add functionality to close button
+    - add functionality to submit via clicking outside form el when either note text/title is keyed in
  */
 
 class App {
   constructor() {
     this.notes = []; // create an empty array to store submitted note objects
+
     // differentiate queries with $ sign means html elements
     // whereas no $ means data
+    // references to elements
+    this.$notes = document.querySelector("#notes");
+    this.$placeholder = document.querySelector("#placeholder");
     this.$form = document.querySelector("#form");
     this.$noteTitle = document.querySelector("#note-title");
     this.$noteText = document.querySelector("#note-text");
@@ -41,10 +55,9 @@ class App {
     const isFormClicked = this.$form.contains(event.target); // contains returns true/false
     if (isFormClicked) {
       this.openForm();
-    } // open form
-    else {
+    } else {
       this.closeForm();
-    } // close form
+    }
   }
 
   openForm() {
@@ -57,8 +70,10 @@ class App {
     this.$form.classList.remove("form-open"); // removes box shadow
     this.$noteTitle.style.display = "none"; // make the el invisible
     this.$formButtons.style.display = "none"; // make el invisible
+    // clear out values in title/text
+    this.$noteTitle.value = "";
+    this.$noteText.value = "";
   }
-
 
   // add note functionality
   addNote(note) {
@@ -71,7 +86,32 @@ class App {
 
     //
     this.notes = [...this.notes, newNote];
-    console.log(this.notes);
+    this.displayNotes();
+    this.closeForm();
+  }
+
+  displayNotes() {
+    // check if notes array has el
+    const hasNotes = this.notes.length > 0;
+    this.$placeholder.style.display = hasNotes ? "none" : "flex";
+
+    this.$notes.innerHTML = this.notes
+      .map(
+        (note) =>
+          `
+      <div style="background: ${note.color};" class="note">
+        <div class="${note.title && "note-title"}">${note.title}</div>
+        <div class="note-text">${note.text}</div>
+        <div class="toolbar-container">
+          <div class="toolbar">
+            <img class="toolbar-color" src="https://icon.now.sh/palette">
+            <img class="toolbar-delete" src="https://icon.now.sh/delete">
+          </div>
+        </div>
+      </div>
+      `
+      )
+      .join("");
   }
 }
 
